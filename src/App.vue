0829b7d3313b8918887229cfec5fce99770c7d5c
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-02-15 11:14:03 +0700

    lession-01
    lession-02

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- interpolation -->
+    <h1>{{title}}</h1>
+    <h2>{{count}}</h2>
+    <button @click="onClickAdd">Add</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:"app",
+  data() {
+    return {
+      title:"Jackmobile www.facebook.com/jackultimate",
+      count:0
+    }
+  },
+  methods: {
+    onClickAdd(){
+      this.count++
+    }
+  },
+};
+</script>
+
+<style>
+</style>
